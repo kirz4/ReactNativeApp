e5e4945a3b02803f29beb5527f33dbab43246e22
kirz4 <lucasrabello098@gmail.com>
2025-01-09 12:04:48 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}", // Inclua os caminhos dos seus arquivos de componentes
+    "./components/**/*.{js,jsx,ts,tsx}", // Caminhos adicionais para componentes
+  ],
+  presets: [require("nativewind/preset")], // Mantendo o preset do NativeWind
+  theme: {
+    extend: {
+      colors: {
+        primary: "#161622",
+        secondary: {
+          DEFAULT: "#FF9C01",
+          100: "#FF9001",
+          200: "#FF8E01",
+        },
+        black: {
+          DEFAULT: "#000",
+          100: "#1E1E2D",
+          200: "#232533",
+        },
+        gray: {
+          100: "#CDCDE0",
+        },
+      },
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+    },
+  },
+  plugins: [], // Mantendo os plugins vazios por enquanto
+};
